2ac15ced4423fe5828e32057d06cda68bf2dd642
Your Actual Name <your.email@example.com>
2026-09-09 22:22:18 +0800

    Fix database config array key to main

--- LavaLust/app/config/database.php
+++ LavaLust/app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-$db['default'] = array(
+$db['main'] = array(
     'hostname' => 'mysql-1ae50f6f-lavalustproject1.c.aivencloud.com',
     'username' => 'avnadmin',
     'password' => 'AVNS_PIE4ft5nL6UkoDtHdqg',
